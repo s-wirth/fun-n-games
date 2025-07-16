1b8790e34e5ee5ac89b8323a4af70a632b5dfd81
Sophie Wirth <a.sophiewirth@gmail.com>
2025-07-16 15:47:41 +0200

    move ball around

--- src/app/components/gameCanvas.tsx
+++ src/app/components/gameCanvas.tsx
@@ -2,6 +2,15 @@
 import styles from "./gameCanvas.module.css";
 import React, { useRef, useEffect, useState } from "react";
 
+function draw(context, ball) {
+  const { x, y, radius, color } = ball;
+  context.beginPath();
+  context.arc(x, y, radius, 0, Math.PI * 2, true);
+  context.fillStyle = color;
+  context.fill();
+  context.closePath();
+}
+
 const GameCanvas = () => {
   /* -------------- SETUP -------------- */
   const CANVAS_META = {
@@ -15,13 +24,24 @@ const GameCanvas = () => {
   const [canvas, setCanvas] = useState(null);
   const [context, setContext] = useState(null);
   const [balls, setBalls] = useState([]);
+  const [inMotion, setInMotion] = useState(false);
+  const [raf, setRaf] = useState(null);
 
   /* -------------- INIT -------------- */
   useEffect(() => {
     const canvas = canvasRef.current;
     setCanvas(canvas);
     setContext(canvas.getContext("2d"));
-    setBalls([{ x: CANVAS_META.width / 2, y: 20, radius: 10, color: "blue" }]);
+    setBalls([
+      {
+        x: CANVAS_META.width / 2,
+        y: 20,
+        vx: 5,
+        vy: 5,
+        radius: 10,
+        color: "blue",
+      },
+    ]);
   }, []);
 
   useEffect(() => {
@@ -36,6 +56,7 @@ const GameCanvas = () => {
       }, 0, 0.5)`;
       context.fillRect(0, 50 * i, CANVAS_META.width, 50);
     }
+
     // Draw spout
     context.beginPath();
     context.arc(canvas.width / 2, 0, 10, 0, Math.PI * 2, true);
@@ -43,16 +64,51 @@ const GameCanvas = () => {
     context.fill();
     context.closePath();
 
+    context.save();
+
     // Draw ball
     balls.forEach((ball) => {
-      const { x, y, radius, color } = ball;
-      context.beginPath();
-      context.arc(x, y, radius, 0, Math.PI * 2, true);
-      context.fillStyle = color;
-      context.fill();
-      context.closePath();
+      draw(context, ball);
     });
   }, [balls, canvas, context]);
+
+  useEffect(() => { 
+    if (!inMotion) {
+      if (raf) {
+        window.cancelAnimationFrame(raf);
+      }
+      return
+    }
+    const animate = () => {
+      context.restore();
+      balls.forEach((ball) => {
+        const ballXBound = ball.x + ball.radius;
+        const ballYBound = ball.y + ball.radius;
+        // ball.x cant be smaller than 0 or larger than canvas width
+        // ball.y cant be smaller than 0 or larger than canvas height
+        if (ballXBound >= CANVAS_META.width || ballXBound <= 0) {
+          console.log('ball outside width, reversing vx')
+          ball.vx *= -1;
+        }
+        if (ballYBound >= CANVAS_META.height || ballYBound <= 0) {
+          console.log('ball outside height, reversing vy')
+          ball.vy *= -1;
+        }
+
+        ball.x += ball.vx;
+        ball.y += ball.vy;
+        console.log('ballXBound', ballXBound)
+        console.log('ballYBound', ballYBound)
+        draw(context, ball);
+      });
+      if (inMotion) {
+        setRaf(requestAnimationFrame(animate));
+      }
+    };
+    if (inMotion) {
+      setRaf(requestAnimationFrame(animate));
+    }
+  }, [inMotion]);
 
   /* -------------- RENDER -------------- */
   return (
@@ -62,6 +118,7 @@ const GameCanvas = () => {
       height={CANVAS_META.height}
       style={{ border: "1px solid black" }}
       className={styles.canvas}
+      onClick={() => setInMotion(!inMotion)}
     />
   );
 };
