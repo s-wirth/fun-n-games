1325a0c78d64a38905193895ebf469e679ede8a5
Sophie Wirth <a.sophiewirth@gmail.com>
2025-07-16 11:26:43 +0200

    setup

---
+++ src/app/components/gameCanvas.tsx
@@ -0,0 +1,61 @@
+'use client';
+import styles from "./gameCanvas.module.css";
+import React, { useRef, useEffect, useState } from "react";
+
+const GameCanvas = () => {
+  /* -------------- SETUP -------------- */
+  const CANVAS_META = {
+    width: 500,
+    height: 750
+  }
+
+  const canvasRef = useRef(null);
+  const [canvas, setCanvas] = useState(null);
+  const [context, setContext] = useState(null);
+  const [balls, setBalls] = useState([]);
+
+  /* -------------- INIT -------------- */
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    setCanvas(canvas);
+    setContext(canvas.getContext("2d"));
+    setBalls([
+      { x: CANVAS_META.width / 2, y: 20, radius: 10, color: "blue" }
+    ])
+  }, []);
+
+  useEffect(() => {
+    if (!canvas || !context) {
+      return;
+    }
+    // Draw spout
+    context.beginPath();
+    context.arc(canvas.width / 2, 0, 10, 0, Math.PI * 2, true);
+    context.fillStyle = "black";
+    context.fill();
+    context.closePath();
+
+    // Draw ball
+    balls.forEach((ball) => {
+      const { x, y, radius, color } = ball;
+      context.beginPath();
+      context.arc(x, y, radius, 0, Math.PI * 2, true);
+      context.fillStyle = color;
+      context.fill();
+      context.closePath();
+    });
+  }, [balls, canvas, context]);
+
+  /* -------------- RENDER -------------- */
+  return (
+    <canvas
+      ref={canvasRef}
+      width={CANVAS_META.width}
+      height={CANVAS_META.height}
+      style={{ border: "1px solid black" }}
+      className={styles.canvas}
+    />
+  );
+};
+
+export default GameCanvas;
