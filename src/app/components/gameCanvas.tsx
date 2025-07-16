0445d1b6155e140d149d77d09a010c7cd4287bc1
Sophie Wirth <a.sophiewirth@gmail.com>
2025-07-16 11:50:03 +0200

    format

--- src/app/components/gameCanvas.tsx
+++ src/app/components/gameCanvas.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import styles from "./gameCanvas.module.css";
 import React, { useRef, useEffect, useState } from "react";
 
@@ -8,8 +8,8 @@ const GameCanvas = () => {
     width: 500,
     height: 750,
     rows: 15,
-    columns: 5
-  }
+    columns: 5,
+  };
 
   const canvasRef = useRef(null);
   const [canvas, setCanvas] = useState(null);
@@ -21,19 +21,20 @@ const GameCanvas = () => {
     const canvas = canvasRef.current;
     setCanvas(canvas);
     setContext(canvas.getContext("2d"));
-    setBalls([
-      { x: CANVAS_META.width / 2, y: 20, radius: 10, color: "blue" }
-    ])
+    setBalls([{ x: CANVAS_META.width / 2, y: 20, radius: 10, color: "blue" }]);
   }, []);
 
   useEffect(() => {
     if (!canvas || !context) {
       return;
     }
+
+    // Display rows
     for (let i = 0; i < CANVAS_META.rows; i++) {
-      context.fillStyle = `rgba(0, ${255 * i / CANVAS_META.rows}, 0, 0.5)`;
-      context.fillRect(0, 50*i, CANVAS_META.width, 50)
-      
+      context.fillStyle = `rgba(0, ${
+        (255 * (i + 1)) / CANVAS_META.rows
+      }, 0, 0.5)`;
+      context.fillRect(0, 50 * i, CANVAS_META.width, 50);
     }
     // Draw spout
     context.beginPath();
